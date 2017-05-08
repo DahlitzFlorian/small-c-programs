673c5925ce4f980725c7d14e61a96605972aadc8
florian dahlitz <f2dahlitz@freenet.de>
2017-05-08 22:53:05 +0200

    improved draw_sine a bit -> far away from perferct

--- src/draw_sine.c
+++ src/draw_sine.c
@@ -29,14 +29,14 @@ int main()
     // x-axis
     LOCATE(RZ, 1);
     for(column = 1; column < 78; ++column)
-        (column - CZ) % 8 ? printf("Ä") : printf("\197"), fflush(stdout);
+        (column - CZ) % 8 ? putchar('|') : putchar('-');
     putchar('\020');
     LOCATE(RZ - 1, CZ + 64);
-    printf("2\227    x");
+    printf("2 PI    x");
 
     // y-axis
     for(row = 6; row < 25; ++row)
-        LOCATE(row, CZ), printf("\197"), fflush(stdout);
+        LOCATE(row, CZ), putchar('-');
     LOCATE(5, CZ);
     printf("\036 sin(x)");
 
